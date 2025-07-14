ca4b7743523cdcfb74f1d7d762e405f30d5c1514
Marlou Dev <limgchurch712@gmail.com>
2025-07-14 17:54:03 +0800

    holiday

---
+++ app/Http/Controllers/HolidayController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Holiday;
+use Illuminate\Http\Request;
+
+class HolidayController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'name' => 'required|string|max:25',
+            'date' => 'required|date|unique:holidays,date',
+            'type' => 'required|string|max:25'
+        ],[
+            'date.unique' => 'This date is already marked as a holiday'
+        ]);
+
+        $holiday = Holiday::create($validateData);
+
+        return response()->json([
+            'message' => 'Device successfully created',
+            'holiday' => $holiday,
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Holiday $holiday)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Holiday $holiday)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Holiday $holiday)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Holiday $holiday)
+    {
+        //
+    }
+}
